bf8c842eb5fb5f2da7b10fbaa41af5d2614d482c
Joseph Ishak <jishak1@asu.edu>
2017-10-11 22:38:00 -0700

    Completed Feed header, feed post and post component

--- SocialMedia/View/CircleView.swift
+++ SocialMedia/View/CircleView.swift
@@ -1,26 +1,25 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SocialMedia
 //
-//  Created by Joseph  Ishak on 10/8/17.
+//  Created by Joseph  Ishak on 10/11/17.
 //  Copyright © 2017 Joseph  Ishak. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5
         layer.shadowOffset = CGSize(width:1, height:1)
-        layer.cornerRadius = 2.0
-        
-        
-        
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
