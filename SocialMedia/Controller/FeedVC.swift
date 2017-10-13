3ac6b0cc6deb3aa179e4fe6cbdbfb96c838884dd
Joseph Ishak <jishak1@asu.edu>
2017-10-12 22:27:22 -0700

    Adjusted the UI Feed

--- SocialMedia/Controller/FeedVC.swift
+++ SocialMedia/Controller/FeedVC.swift
@@ -10,25 +10,32 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate  = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     @IBAction func signOutTapped(_ sender: Any) {
-        let keychainresult: Bool = KeychainWrapper.standard.remove(key: "\(KEY_UID)")
+        let keychainresult: Bool = KeychainWrapper.standard.removeObject(forKey: "\(KEY_UID)")
         print("Joe: ID Removed from keychain \(keychainresult)" )
         try! Auth.auth().signOut()
         performSegue(withIdentifier: "goToSignIn", sender: nil)
     }
-    
+
     
 }
---
+++ SocialMedia/View/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  SocialMedia
+//
+//  Created by Joseph  Ishak on 10/12/17.
+//  Copyright © 2017 Joseph  Ishak. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg:     UIImageView!
+    @IBOutlet weak var caption:      UITextView!
+    @IBOutlet weak var likesLbl:    UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+    
+    
+  
+}
