7eaf545ee6aba7feccdbd4acf300b71c9ae49e99
Joseph Ishak <jishak1@asu.edu>
2017-10-12 23:53:28 -0700

    completed created firebase db users

--- SocialMedia/Controller/SignInVC.swift
+++ SocialMedia/Controller/SignInVC.swift
@@ -56,7 +56,8 @@ class SignInVC: UIViewController {
             }else{
                 print("Joe: Succesfully Authenticated with FireBase")
                 if let user = user{
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider":credential.provider]
+                    self.completeSignIn(id: user.uid,userData: userData)
                 
                 }
                 
@@ -71,7 +72,8 @@ class SignInVC: UIViewController {
                 if error == nil{
                     print("Joe: Email authentication with firebase sucessful!")
                     if let user = user{
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider":user.providerID]
+                        self.completeSignIn(id: user.uid,userData: userData)
                     }
                     
                 }else{
@@ -81,7 +83,8 @@ class SignInVC: UIViewController {
                         }else{
                             print("Joe: Sucessfully authenticated with firebase")
                             if let user = user{
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider":user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                             
                         }
@@ -91,7 +94,11 @@ class SignInVC: UIViewController {
             
         }
     }
-    func completeSignIn(id: String){
+    func completeSignIn(id: String,userData:Dictionary<String,String>){
+       
+        let ds = DataServices()
+        
+        ds.createFirebaseDBUser(uid: id, userData:userData)
         let keychainresult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("Joe: Data saved to keychain \(keychainresult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
---
+++ SocialMedia/Services/DataServices.swift
@@ -0,0 +1,42 @@
+//
+//  DataServices.swift
+//  SocialMedia
+//
+//  Created by Joseph  Ishak on 10/12/17.
+//  Copyright © 2017 Joseph  Ishak. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+class DataServices{
+    
+    //singleton is an instance of a class is a single instance for all classes referencing
+    static let ds = DataServices()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_POSTS = DB_BASE.child("posts")
+    private var _REF_USERS = DB_BASE.child("users")
+    
+    var REF_BASE:DatabaseReference{
+        return _REF_BASE
+    }
+    
+    var REF_POSTS:DatabaseReference{
+        return _REF_POSTS
+    }
+    var REF_USERS:DatabaseReference{
+        return _REF_USERS
+    }
+    
+    
+    func createFirebaseDBUser(uid: String,userData:Dictionary<String,String>){
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+    
+    
+    
+    
+}
