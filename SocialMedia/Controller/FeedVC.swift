2776c28ae0c3748ec1bd2f52a0a612eafd5f0c5c
Joseph Ishak <jishak1@asu.edu>
2017-10-13 00:40:45 -0700

    completed Get requests to the Firebase Database

--- SocialMedia/Controller/FeedVC.swift
+++ SocialMedia/Controller/FeedVC.swift
@@ -17,16 +17,22 @@ class FeedVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-               tableView.delegate = self
+        
+       tableView.delegate = self
        tableView.dataSource = self
-        // Do any additional setup after loading the view.
+        
+        DataServices.ds.REF_POSTS.observe(.value) { (snapshot) in
+            print(snapshot.value!)
+            
+        }
+        
     }
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
